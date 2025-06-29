5c8e8c10d690cb48be1e06fe484977295f10bd87
Eros-Pinzani <eros.pinzani@edu.unifi.it>
2025-06-29 11:46:17 +0200

    Commit for deployment

--- create_superuser.py
+++ create_superuser.py
@@ -1,8 +1,7 @@
 import os
 import django
 
-# Imposta la variabile d'ambiente per il settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ecommerce.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Ecommerce.settings")  # Cambia con il tuo progetto
 django.setup()
 
 from django.contrib.auth import get_user_model
@@ -12,9 +11,19 @@ User = get_user_model()
 username = "admin"
 email = "eros.pinzani@edu.unifi.it"
 password = "190568Ab@"
+first_name = "Eros"
+last_name = "Pinzani"
 
 if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
-    print("Superuser creato.")
+    User.objects.create_superuser(
+        username=username,
+        email=email,
+        password=password,
+        first_name=first_name,
+        last_name=last_name
+    )
+    with open("superuser_created.txt", "w") as f:
+        f.write("✅ Superuser creato.\n")
 else:
-    print("Superuser già esistente.")
+    with open("superuser_created.txt", "w") as f:
+        f.write("⚠️ Superuser già esistente.\n")
